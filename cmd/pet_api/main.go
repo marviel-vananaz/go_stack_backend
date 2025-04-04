bb6e02fd657e6cb3233a2e0add6c802fe5833d5d
marviel-vananaz <marviel@vananaz.com>
2025-04-04 12:46:19 +0800

    Basic auth for Backend

--- cmd/pet_api/main.go
+++ cmd/pet_api/main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
+	firebase "firebase.google.com/go/v4"
 	"github.com/marviel-vananaz/go-stack-backend/.gen/api"
 	"github.com/marviel-vananaz/go-stack-backend/infra/sqlite"
 	"github.com/marviel-vananaz/go-stack-backend/usecase/petsvc"
 	_ "github.com/mattn/go-sqlite3"
+	"google.golang.org/api/option"
 )
 
 func main() {
+	// Initialize Firebase Admin SDK
+	opt := option.WithCredentialsFile("./firebase_config.json") // Update this path
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		log.Fatalf("error initializing firebase app: %v\n", err)
+	}
+
+	authClient, err := app.Auth(context.Background())
+	if err != nil {
+		log.Fatalf("error getting Auth client: %v\n", err)
+	}
+
 	// Create service instance.
 	db, err := sql.Open("sqlite3", "../database/database.db")
 	if err != nil {
@@ -34,6 +50,23 @@ func main() {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
+
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Authorization header required", http.StatusUnauthorized)
+			return
+		}
+
+		idToken := strings.TrimPrefix(authHeader, "Bearer ")
+
+		token, err := authClient.VerifyIDToken(r.Context(), idToken)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Invalid token: %s", token), http.StatusUnauthorized)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "userId", token.UID)
+		r = r.WithContext(ctx)
 
 		srv.ServeHTTP(w, r)
 	})
